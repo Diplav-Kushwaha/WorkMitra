83795f5d2c8e3bbab7aa8e7dfb3088eed344d7a1
Diplav Kushwaha <diplavkushwaha@gmail.com>
2026-03-10 14:28:57 +0530

    Update User.java

--- User.java
+++ User.java
@@ -8,4 +8,13 @@ public class User {
     private String name;
     private Role role;
 
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private WorkerProfile workerProfile;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Organization organization;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Message> message=new ArrayList<>();
+
 }
