dbd700e02d5378b40c465df3e7c0f869a4a9a896
Diplav Kushwaha <diplavkushwaha@gmail.com>
2026-03-10 15:17:47 +0530

    Update User.java

--- User.java
+++ User.java
@@ -3,9 +3,19 @@
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Pattern(regexp = "^[+]?[0-9]{10}$")
+    @Column(nullable = false, unique = true, length = 15)
     private String phoneNumber;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -16,5 +26,11 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Message> message=new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserPost> userPosts=new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WorkerPost> workerPosts=new ArrayList<>();
 
 }
